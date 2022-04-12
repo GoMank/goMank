ec33beacd8fec03a09302abc84be3debb8f330cd
Muhammad Khairul Rizal Purwanto <rizal.purwanto.id@gmail.com>
2022-04-12 22:09:13 +0700

    setup for react web

---
+++ client-web/client-web-app/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'gomank-blue': '#0386ee',
+      'gomank-yellow': '#FEC900',
+      'gomank-gray': '#E1E1E1',
+      'gomank-white': '#F6F6F6'
+    }
+  },
+  plugins: [],
+}
