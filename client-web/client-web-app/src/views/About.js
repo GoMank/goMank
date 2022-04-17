09eb039a9d770b392a7251599f9f4d633e2115dd
Muhammad Khairul Rizal Purwanto <rizal.purwanto.id@gmail.com>
2022-04-17 21:23:05 +0700

    pulled

--- client-web/client-web-app/src/views/About.js
+++ client-web/client-web-app/src/views/About.js
@@ -2,79 +2,95 @@
 export default function About() {
     return (
         <div>
-            
+
             <div className="bg-white grid grid-cols-2 justify-items-center">
-                
+
                 <div className="rounded-lg flex items-center px-7 py-7">
                     <img className="rounded-full" src={require('../about-img.jpg')}></img>
                 </div>
                 <div className="px-2 py-9 min-w-md">
                     <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Tentang GoMank</h1>
-                    <p className='text-justify py-3 px-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et diam ut elit consequat aliquet. Cras tincidunt, elit in tincidunt rutrum, enim mauris aliquam ante, nec tincidunt turpis urna id ante. Duis pulvinar orci a lectus aliquet, vel hendrerit justo sollicitudin. Sed ultrices urna sapien, et mattis orci egestas et. Fusce accumsan viverra orci, nec facilisis nunc scelerisque et. Nunc maximus, lorem ac sodales tempus, ligula risus volutpat lectus, et facilisis massa urna sed nisl. Suspendisse eget quam sit amet dolor luctus auctor vel et diam. Etiam augue nibh, ullamcorper eu metus vel, lacinia molestie arcu. Vestibulum faucibus diam vel ante egestas, laoreet mollis est hendrerit. Curabitur placerat erat ut euismod viverra. Donec ornare arcu nec sem imperdiet iaculis. Proin sit amet arcu in nunc auctor pellentesque ut quis ipsum. Mauris dictum aliquam tortor et aliquam. Nulla a porta purus, quis vulputate dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec bibendum, dui id tincidunt ornare, ante lectus congue enim, id tempor ipsum felis vitae turpis.</p>
+                    <p className='text-justify py-3 px-3'>GoMank adalah jasa Cuci Mobil di Rumah berlangganan yang menawarkan berbagai layanan cuci mobil panggilan Jakarta, cuci mobil panggilan Bandung, cuci mobil panggilan Surabaya dengan paket lengkap berkualitas standar car wash profesional.
+
+                        GoMank hadir sebagai automotive car wash yang berfokus pada layanan membersihkan mobil, detailing mobil, fogging mobil hingga cuci interior mobil. Kini, Anda tak perlu repot lagi mencari salon mobil terbaik di Jakarta atau poles mobil terdekat.
+
+                        GoMank memberikan pelayanan cuci mobil berlangganan terbaik agar Anda merasa memiliki car wash profesional pribadi di rumah. </p>
 
                 </div>
-                
+
 
             </div>
             <div className="bg-white grid grid-cols-2 justify-items-center">
-                
+
+
+                <div className="px-2 py-9 min-w-md">
+                    <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Sejarah GoMank</h1>
+                    <p className='text-justify py-3 px-3'>GoMank sudah beroperasi selama dua tahun, yaitu sejak bulan September tahun 2019 lalu. Nama GoMank sendiri diambil dari dua kata yaitu Mobi dari kata mobil atau mobile yang dapat diartikan sebagai jasa cuci mobil atau car detailing yang fleksibel dan bisa berpindah dengan mudah, sedangkan Klin yang diambil dari kata clean alias bersih.
+
+                        Dari nama ini menggambarkan bahwa GoMank merupakan jasa cuci panggilan berlangganan yang memiliki kualitas salon mobil terbaik di kelasnya. Awal didirikannya GoMank bermula dari kebutuhan pribadi pendirinya yang ingin memiliki mobil yang selalu bersih tanpa repot-repot mengantri di car wash konvensional.
+
+                        Tak hanya cuci mobil biasa, ketika itu ia ingin mendapatkan layanan cuci sekaligus poles mobil di rumah yang lebih personal. Pendiri GoMank menganggap urusan kebersihan mobil bukan hal yang sepele dan harus dikerjakan secara detail agar mobil terawat maksimal. Atas ide ini lah GoMank akhirnya resmi terbentuk pada tahun 2019 lalu.</p>
+
+                </div>
                 <div className="rounded-lg px-7 flex items-center py-7">
                     <img className="rounded-2xl px-3 py-3" src={require('../history-img.png')}></img>
                 </div>
-                <div className="px-2 py-9 min-w-md">
-                    <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Sejarah GoMank</h1>
-                    <p className='text-justify py-3 px-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et diam ut elit consequat aliquet. Cras tincidunt, elit in tincidunt rutrum, enim mauris aliquam ante, nec tincidunt turpis urna id ante. Duis pulvinar orci a lectus aliquet, vel hendrerit justo sollicitudin. Sed ultrices urna sapien, et mattis orci egestas et. Fusce accumsan viverra orci, nec facilisis nunc scelerisque et. Nunc maximus, lorem ac sodales tempus, ligula risus volutpat lectus, et facilisis massa urna sed nisl. Suspendisse eget quam sit amet dolor luctus auctor vel et diam. Etiam augue nibh, ullamcorper eu metus vel, lacinia molestie arcu. Vestibulum faucibus diam vel ante egestas, laoreet mollis est hendrerit. Curabitur placerat erat ut euismod viverra. Donec ornare arcu nec sem imperdiet iaculis. Proin sit amet arcu in nunc auctor pellentesque ut quis ipsum. Mauris dictum aliquam tortor et aliquam. Nulla a porta purus, quis vulputate dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec bibendum, dui id tincidunt ornare, ante lectus congue enim, id tempor ipsum felis vitae turpis.</p>
 
-                </div>
-                
 
             </div>
-            
+
             <div className="bg-white grid grid-cols-2 justify-items-center">
-                
+
                 <div className="rounded-lg px-7 py-7">
                     <img className="rounded-full h-2/3" src={require('../moto-img.jpg')}></img>
                 </div>
                 <div className="px-2 py-9 min-w-md">
                     <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Tujuan GoMank</h1>
-                    <p className='text-justify py-3 px-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et diam ut elit consequat aliquet. Cras tincidunt, elit in tincidunt rutrum, enim mauris aliquam ante, nec tincidunt turpis urna id ante. Duis pulvinar orci a lectus aliquet, vel hendrerit justo sollicitudin. Sed ultrices urna sapien, et mattis orci egestas et. Fusce accumsan viverra orci, nec facilisis nunc scelerisque et. Nunc maximus, lorem ac sodales tempus, ligula risus volutpat lectus, et facilisis massa urna sed nisl. Suspendisse eget quam sit amet dolor luctus auctor vel et diam. Etiam augue nibh, ullamcorper eu metus vel, lacinia molestie arcu. Vestibulum faucibus diam vel ante egestas, laoreet mollis est hendrerit. Curabitur placerat erat ut euismod viverra. Donec ornare arcu nec sem imperdiet iaculis. Proin sit amet arcu in nunc auctor pellentesque ut quis ipsum. Mauris dictum aliquam tortor et aliquam. Nulla a porta purus, quis vulputate dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec bibendum, dui id tincidunt ornare, ante lectus congue enim, id tempor ipsum felis vitae turpis.</p>
+                    <p className='text-justify py-3 px-3'>Tujuan utama GoMank adalah menyediakan layanan cuci mobil berlangganan yang lebih nyaman kepada pelanggan. GoMank berfokus pada customer yang aware terhadap kondisi kendaraannya dan ingin mendapatkan pelayanan yang maksimal.
+
+                        Selain menyediakan berbagai layanan cuci mobil di rumah yang bisa dipilih berdasarkan paket berlangganan, GoMank juga ingin berkontribusi secara aktif kepada banyak orang, terutama bagi mereka yang memiliki keahlian di bidang jasa cuci mobil, detailing mobil dan urusan perawatan kendaraan. GoMank membuka lapangan kerja yang lebih luas kepada mereka yang memiliki keahlian tersebut.</p>
 
                 </div>
-                
+
 
             </div>
 
             <div className="bg-white grid grid-cols-2 justify-items-center">
-                
+
+
+                <div className="px-2 py-9 min-w-md">
+                    <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Wilayah Kerja GoMank</h1>
+                    <p className='text-justify py-3 px-3'>Hingga saat ini, GoMank telah beroperasi di empat kota besar di Indonesia, yaitu Jakarta, Tangerang, Bandung dan Surabaya. Didukung oleh tim yang terlatih, GoMank selalu siap menerima jasa cuci mobil panggilan sekaligus salon mobil Bandung, salon mobil Surabaya, salon mobil Jakarta dan di sekitar wilayah kerja utamanya.
+
+Untuk kualitas pekerjaan, jangan khawatir, GoMank selalu membekali setiap SDM/operatornya dengan Standard Operational Procedure (SOP) yang sama di setiap kota yang menjadi wilayah kerja GoMank. Sebelum mempekerjakan operator cuci mobil, GoMank mengadakan seleksi ketat kepada mereka yang memiliki pengalaman di bidang cuci mobil dan detailing mobil, sehingga kualitas pekerjaannya pun dijamin profesional.</p>
+
+                </div>
                 <div className="rounded-lg px-7 flex items-center py-7">
                     <img className="rounded-lg" src={require('../map-img.png')}></img>
                 </div>
-                <div className="px-2 py-9 min-w-md">
-                    <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Wilayah Kerja GoMank</h1>
-                    <p className='text-justify py-3 px-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et diam ut elit consequat aliquet. Cras tincidunt, elit in tincidunt rutrum, enim mauris aliquam ante, nec tincidunt turpis urna id ante. Duis pulvinar orci a lectus aliquet, vel hendrerit justo sollicitudin. Sed ultrices urna sapien, et mattis orci egestas et. Fusce accumsan viverra orci, nec facilisis nunc scelerisque et. Nunc maximus, lorem ac sodales tempus, ligula risus volutpat lectus, et facilisis massa urna sed nisl. Suspendisse eget quam sit amet dolor luctus auctor vel et diam. Etiam augue nibh, ullamcorper eu metus vel, lacinia molestie arcu. Vestibulum faucibus diam vel ante egestas, laoreet mollis est hendrerit. Curabitur placerat erat ut euismod viverra. Donec ornare arcu nec sem imperdiet iaculis. Proin sit amet arcu in nunc auctor pellentesque ut quis ipsum. Mauris dictum aliquam tortor et aliquam. Nulla a porta purus, quis vulputate dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec bibendum, dui id tincidunt ornare, ante lectus congue enim, id tempor ipsum felis vitae turpis.</p>
 
-                </div>
-                
 
             </div>
 
             <div className="bg-white grid grid-cols-2 justify-items-center">
-                
+
                 <div className="rounded-lg px-7 py-7 flex items-center">
                     <img className="rounded-full " src={require('../premium-img.jpg')}></img>
                 </div>
                 <div className="px-2 py-9 min-w-md">
                     <h1 className="text-3xl pb-3 mx-4 text-left font-bold text-gray-600">Keunggulan GoMank Dibanding Jasa Cuci Mobil Panggilan Lain?</h1>
-                    <p className='text-justify py-3 px-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et diam ut elit consequat aliquet. Cras tincidunt, elit in tincidunt rutrum, enim mauris aliquam ante, nec tincidunt turpis urna id ante. Duis pulvinar orci a lectus aliquet, vel hendrerit justo sollicitudin. Sed ultrices urna sapien, et mattis orci egestas et. Fusce accumsan viverra orci, nec facilisis nunc scelerisque et. Nunc maximus, lorem ac sodales tempus, ligula risus volutpat lectus, et facilisis massa urna sed nisl. Suspendisse eget quam sit amet dolor luctus auctor vel et diam. Etiam augue nibh, ullamcorper eu metus vel, lacinia molestie arcu. Vestibulum faucibus diam vel ante egestas, laoreet mollis est hendrerit. Curabitur placerat erat ut euismod viverra. Donec ornare arcu nec sem imperdiet iaculis. Proin sit amet arcu in nunc auctor pellentesque ut quis ipsum. Mauris dictum aliquam tortor et aliquam. Nulla a porta purus, quis vulputate dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec bibendum, dui id tincidunt ornare, ante lectus congue enim, id tempor ipsum felis vitae turpis.</p>
+                    <p className='text-justify py-3 px-3'>Perbedaan utama adalah kualitas, kami menjamin bahwa pekerjaan yang kami lakukan berdasarkan SOP yang dirancang khusus untuk kualitas cuci mobil yang lebih optimal. Kami selalu menggunakan cairan pembersih premium dari merek-merek ternama seperti Meguiar's, Kit dan Menzerna. Kami menjamin tim kami tidak akan menggunakan cairan oplosan atau sampo cuci mobil curah yang berpengaruh pada kualitas hasil cuci mobil yang kami kerjakan.
+
+Selain menggunakan cairan pembersih premium, kami mengutamakan layanan cuci mobil berlangganan. Opsi berlangganan ini kami tawarkan untuk mengubah mind set customer tentang membersihkan mobil yang hanya dilakukan sekali ketika mobil benar-benar kotor. Konsep cuci mobil berlangganan ini pun disediakan lewat berbagai paket yang bisa dipilih dan diatur sesuai keinginan customer.</p>
 
                 </div>
-                
+
 
             </div>
-            
+
 
             <div className="bg-gomank-yellow h-6">
-                
+
 
             </div>
 
