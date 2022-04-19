d6acdbf69f873bc504cb24ef7b69952589b55cfb
ignatius andri pratama putra <ignaway@gmail.com>
2022-04-20 02:15:59 +0700

    update log

--- client-mobile/src/screens/LogPage.js
+++ client-mobile/src/screens/LogPage.js
@@ -7,59 +7,34 @@ export default function LogPage() {
  
   if(loading) {
     return (
-      <View style={styles.container} >
-      
-
+    <View style={styles.container} >
       <View nestedScrollEnabled={true} >
-       
-        
-
         <ScrollView >
           <View >
           <Text>Loading .....</Text>
           </View>
-
-
-
-          
-
-
-
         </ScrollView>
-
-
       </View>
-
     </View>
-    )
-    
-  }
+    )}
   if(error) {
     return (
-      <View style={styles.container}>
-      
+    <View style={styles.container}>
       <View nestedScrollEnabled={true} >
-       
         <ScrollView>
           <View >
-          <Text >Error:  {error.message}</Text>
+          <Text >Error: {error.message}</Text>
           </View>
-
         </ScrollView>
-
-
       </View>
-
     </View>
     )
   } 
   console.log(data.histories, "INI HISTORY")
   return (
-    <ScrollView contentContainerStyle={ {alignItems: "center"}} style={{flex: 1,
-      backgroundColor: "#E5E5E5",
-     }}>
+    <ScrollView contentContainerStyle={ {alignItems: "center"}} style={{flex: 1, backgroundColor: "#E5E5E5",}}>
       <View style={{ marginTop: 20 }} />
-      <View style={styles.card}>
+      {/* <View style={styles.card}>
         <View style={{ justifyContent: "center" }}>
           <Image source={require("../../assets/LogoGomank.png")} style={styles.Image} />
         </View>
@@ -68,10 +43,10 @@ export default function LogPage() {
           <Text style={styles.subTitle}>No: 2019100007</Text>
           <Text style={styles.description}>Your booking status no. 2019100007 has been canceled</Text>
         </View>
-      </View>
-      {data.histories.map(history => {
+      </View> */}
+      {data.histories.map((history,index) => {
           return (
-            <View style={styles.card}>
+            <View style={styles.card} key={index}>
         <View style={{ justifyContent: "center" }}>
           <Image source={require("../../assets/LogoGomank.png")} style={styles.Image} />
         </View>
