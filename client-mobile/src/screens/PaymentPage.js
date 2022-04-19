152a423d3254036b42c873e41dca66168723d184
Muhammad Khairul Rizal Purwanto <rizal.purwanto.id@gmail.com>
2022-04-20 00:46:13 +0700

    order create

--- client-mobile/src/screens/PaymentPage.js
+++ client-mobile/src/screens/PaymentPage.js
@@ -5,6 +5,7 @@ import { WebView } from 'react-native-webview';
 import SandboxTest from "./SandboxTest";
 import axios from 'axios';
 
+
 export default function PaymentPage(order) {
     const dataOrder = order.route.params
     console.log("🚀 ~ file: PaymentPage.js ~ line 7 ~ PaymentPage ~ dataOrder", dataOrder)
@@ -12,30 +13,16 @@ export default function PaymentPage(order) {
 
     const [paymentInput, setPayment] = useState('');
     console.log(paymentInput, "INI PAYMENT INPUT")
-    const [addProduct] = useMutation(LOGIN);
-    const submitLogin2 = () => {
-        addProduct({
-            variables: {
-                clientId: $clientId, 
-                mamangId: $mamangId, 
-                service: $service, 
-                date: $date, 
-                time: $time, 
-                address: $address, 
-                paymentMethod: $paymentMethod
-            },
-        });
-        
-    };
+    
 
     async function xendit() {
         const data = await axios.post('https://2be5-125-164-21-106.ngrok.io/xenditPay')
-        return navigation.navigate('SandboxTest', { urlData: data.data })
+        return navigation.navigate('SandboxTest', { urlData: data.data, order: dataOrder, paymentInput: paymentInput })
     }
 
     async function midTrans() {
         const data = await axios.post(`https://6305-114-4-213-204.ngrok.io/orders/midTransPay`)
-        return navigation.navigate('SandboxTest', { urlData: data.data })
+        return navigation.navigate('SandboxTest', { urlData: data.data, order:dataOrder, paymentInput: paymentInput })
     }
 
     return (
