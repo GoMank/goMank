641ed349d7adbeb61d9e742d0526fe46b06e2a8b
ignatius andri pratama putra <ignaway@gmail.com>
2022-04-21 00:49:47 +0700

    pull dari zaki

--- client-mobile/config/apollo.js
+++ client-mobile/config/apollo.js
@@ -1,11 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-<<<<<<< HEAD
-    uri: 'https://old-falcon-74.loca.lt/',
-=======
     uri: 'https://old-falcon-74.loca.lt',
->>>>>>> 4271c2300cc12fcd208451e5f7098bfbb84f1336
     cache: new InMemoryCache(),
 });
 
