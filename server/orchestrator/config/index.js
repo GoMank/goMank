707460e1828df2660e290607883401835e7b7a1c
Rio Lukman Tawekal <riolukmant@gmail.com>
2022-04-21 01:14:08 +0700

    add fild docker

--- server/orchestrator/config/index.js
+++ server/orchestrator/config/index.js
@@ -1,4 +1,8 @@
 const Redis = require('ioredis');
-const redis = new Redis();
+const redis = new Redis({
+    port: 18506,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD,
+});
 
 module.exports = redis;
